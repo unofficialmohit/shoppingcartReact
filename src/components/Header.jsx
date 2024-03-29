7bcc25c45041d3e2d95419d6af3fc435b073e0ac
mohit <mohitgujjar2121@gmail.com>
2024-03-29 16:58:45 +0530

    added badge

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
-import {Button} from "@mui/material";
+import {Badge, Button} from "@mui/material";
 import CartLogo from "@mui/icons-material/ShoppingCart"
+import {  useSelector } from 'react-redux';
 function Header(props) {
+const itemCount=useSelector(state=>state.cart.length);
   return (
     <div>
 
    <nav>    
         <div className='site-name'>INDIA STORE</div>
-        <Button variant='contained' startIcon={<CartLogo/>} onClick={()=>props.setDisplayCart(prevData=>!prevData)}>CART</Button>
+        <Badge badgeContent={itemCount} color='success'>
+            <Button variant='contained' startIcon={<CartLogo/>} onClick={()=>props.setDisplayCart(prevData=>!prevData)}>CART</Button>
+        </Badge>
    </nav>
     </div>
   )
